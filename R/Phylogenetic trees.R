ce4cf044c7fde2ca2d90dfa1f8aead3c371a76d2
FernandaFrozza <100881918+FernandaFrozza@users.noreply.github.com>
2026-01-08 09:58:48 -0300

    Add files via upload

---
+++ R/Phylogenetic trees.R
@@ -0,0 +1,13 @@
+paleta:
+  sample_colors <- c(
+    "Blood Pul122"         = "#D73027", "Blood Pul117" = "pink", "Blood Pul119" = "darkred","Blood Pul123" = "red" ,
+    "Tumor Pul123"         = "#762A83",  
+    "Tumor Pul093"         = "#9970AB",  "Tumor Pul117" = "purple",
+    "dLNs Pul117"          = "#1A9850",  
+    "dLNs Pul093"          = "#66BD63", "dLNs Pul119" = "lightgreen", 
+    "Adjacent tissue Pul117" = "#4575B4" ,"Adjacent tissue Pul093" = "lightblue" ,"Germline" = "black"
+  )
+  
+  
+  sample_colors <- c(
+    "Blood" = "#D73027", "Tumor" = "#762A83", "dLN" = "#1A9850", "Adj Lung" = "#4575B4" ,"Germline" = "black")
